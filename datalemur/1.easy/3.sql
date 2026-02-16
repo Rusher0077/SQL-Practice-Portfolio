41b5853cc2e0a9982909ed2b1c2124de11a70f89
Pallab Dey <pallabdey21@gmail.com>
2026-02-16 21:27:49 +0600

    refractor(DataLemur/Easy): Add solve for Pages With No Likes

--- datalemur/1.easy/3.sql
+++ datalemur/1.easy/3.sql
@@ -2,12 +2,11 @@
 -- find pages that have zero user likes
 SELECT page_id
 FROM (
-        SELECT DISTINCT p.page_id,
-            COUNT(pl.user_id) OVER (
-                PARTITION BY p.page_id
-            ) AS likes
+        SELECT p.page_id,
+            COUNT(pl.user_id) AS cnt
         FROM pages p
             LEFT JOIN page_likes pl ON pl.page_id = p.page_id
+        GROUP BY p.page_id
     ) t
-WHERE likes = 0
+WHERE cnt = 0
 ORDER BY page_id
