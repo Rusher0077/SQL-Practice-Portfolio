80b894ceb5e759baf14a5f487e3a48aa02b8fc0f
Pallab Dey <pallabdey21@gmail.com>
2026-03-10 22:52:10 +0600

    feat(StrataScratch/Medium): Add solve for New Products

---
+++ StrataScratch/2.medium/2.sql
@@ -0,0 +1,23 @@
+-- New Products (ID 10318)
+-- Calculate the net change in the number of products launched by each company in 2020 compared to 2019.
+-- Return the company_name and the net difference (2020 count minus 2019 count).
+
+WITH prod_2020 AS (
+    SELECT company_name,
+        COUNT(product_name) AS net_products
+    FROM car_launches
+    WHERE YEAR = 2020
+    GROUP BY company_name
+),
+prod_2019 AS (
+    SELECT company_name,
+        COUNT(product_name) AS net_products
+    FROM car_launches
+    WHERE YEAR = 2019
+    GROUP BY company_name
+)
+SELECT p2.company_name,
+    (p2.net_products - p1.net_products) AS net_diff
+FROM prod_2020 AS p2
+    LEFT JOIN prod_2019 p1 ON p1.company_name = p2.company_name -- inner join is also working but left join is safe to use cause what if the company
+    -- from 2019 gets discontinued next year
