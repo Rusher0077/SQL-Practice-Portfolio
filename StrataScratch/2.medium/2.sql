8fc5b51621926cb9047f7cd0faf1f25fd151346b
Pallab Dey <pallabdey21@gmail.com>
2026-03-13 22:21:40 +0600

    feat(StrataScratch/Medium): Add alternative approach for New Products

--- StrataScratch/2.medium/2.sql
+++ StrataScratch/2.medium/2.sql
@@ -19,5 +19,23 @@ prod_2019 AS (
 SELECT p2.company_name,
     (p2.net_products - p1.net_products) AS net_diff
 FROM prod_2020 AS p2
-    LEFT JOIN prod_2019 p1 ON p1.company_name = p2.company_name -- inner join is also working but left join is safe to use cause what if the company
-    -- from 2019 gets discontinued next year
+    LEFT JOIN prod_2019 p1 ON p1.company_name = p2.company_name
+     
+-- inner join is also working but left join is safe to use cause what if the company
+-- from 2019 gets discontinued next year
+
+-- Alternative Approach
+select company_name,
+    sum(
+        case
+            when year = 2020 then 1
+            else 0
+        end
+    ) - sum(
+        case
+            when year = 2019 then 1
+            else 0
+        end
+    ) as net_diff
+from car_launches
+group by company_name
