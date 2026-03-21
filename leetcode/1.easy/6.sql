d41db17597add248687d549f3c344d61e1c7ba1e
Pallab Dey <pallabdey21@gmail.com>
2026-03-21 23:16:34 +0600

    feat(LeetCode/Easy): Add solve for Game Play Analysis I

---
+++ leetcode/1.easy/6.sql
@@ -0,0 +1,7 @@
+-- Game Play Analysis I
+-- Write a solution to find the first login date for each player.
+
+SELECT player_id,
+    MIN(event_date) AS first_login
+FROM activity
+GROUP BY player_id
